c56b5e27f4bef65368da118db801d2f1cb2646ff
Bhanu rai <94774049+bhanurai@users.noreply.github.com>
2024-08-12 17:11:59 +0545

    update

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -1,6 +1,7 @@
 const Favorite = require("../model/favoriteModel");
 const User = require("../model/userModel");
 
+
 const addToFavorites = async (req, res) => {
   try {
     const { userId, productId } = req.body;
